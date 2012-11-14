9b3d4ca4b3c8fbb6959d16f02588520029afb925
Edo Mangelaars <e.c.mangelaars@students.uu.nl>
2012-11-14 16:52:51 +0100

    http://codereview.stackexchange.com/questions/18497/hangman-is-this-small-haskell-game-clean-and-idiomatic-haskell-code

--- hangman.hs
+++ hangman.hs
@@ -11,97 +11,57 @@ main = do hSetBuffering stdin NoBuffering
           startplaying $ lines f
 
 startplaying :: [String] -> IO ()
-startplaying words = do index <- randomRIO (0,length words)
-                        let beg = beginstate $ words !! index
-                        playgame beg
-                        putStrLn $ strings lang "Another"
+startplaying words = do index <- randomRIO (0,length words - 1)
+                        playgame (words !! index) []
+                        putStrLn $ strings lang Another
                         ans   <- getChar
                         case ans of
                            'n' -> return ()
                            _   -> startplaying words
 
-playgame :: State -> IO ()
-playgame state
-   | condition state == AlreadyGuessed = newguess
-   | condition state == InvalidLetter  = newguess
-   | condition state == Win            = do putStrLn $ formatState state
-                                            putStrLn $ strings lang "Won"
-                                            return ()
-   | condition state == Loss           = do putStrLn $ formatState state
-                                            putStrLn $ (strings lang "Lost") ++ word state
-                                            return ()
-   | condition state == Okay           = newguess
-   where newguess = do putStrLn $ formatState state
-                       putStrLn $ "\n" ++ (strings lang "Pick")
-                       l <- getChar
-                       putStr "\n"
-                       playgame $ turn state l
+playgame :: String -> [Char] -> IO ()
+playgame word guessed
+  | complete                                       = printState word guessed Won ""
+  | guessedwrong word guessed >= length hangman -1 = printState word guessed Lost word
+  | otherwise                                      = do printState word guessed Pick ""
+                                                        l <- fmap toUpper getChar
+                                                        let guessed' | not (isAlpha l)  = guessed
+                                                                     | l `elem` guessed = guessed
+                                                                     | otherwise        = l : guessed
+                                                        playgame word guessed'
+  where complete :: Bool
+        complete = all (`elem` guessed) (map toUpper word)
 
-formatState :: State -> String
-formatState state = "\ESC[2J" ++ 
-                    unlines [ hangman !! tries state
-                            , showprogress
-                            , (strings lang "Used") ++ intersperse ' ' (guessed state)
-                            ]
-   where showprogress = zipWith (\x y -> if y then x else '_') (word state) (progress state)
+guessedwrong :: String -> [Char] -> Int
+guessedwrong word guessed = length $ filter (`notElem` map toUpper word) guessed
 
-beginstate :: String -> State
-beginstate word = State { condition = Okay
-                        , word      = word
-                        , progress  = replicate (length word) False
-                        , tries     = 0
-                        , guessed   = []
-                        }
+printState :: String -> [Char] -> Message -> String -> IO ()
+printState word guessed message string = putStrLn $ "\ESC[2J" ++
+                                  unlines [ hangman !! (guessedwrong word guessed)
+                                          , map (\x -> if (elem (toUpper x) guessed) then x else '_') word
+                                          , (strings lang Used) ++ intersperse ' ' guessed
+                                          , strings lang message ++ string
+                                          ]
 
+strings :: Language -> Message -> String
+strings NL m = case m of
+                  Another -> "Wil je nog een keer spelen? [Y/n]"
+                  Won     -> "Gefeliciteerd! Je hebt het woord geraden!"
+                  Lost    -> "Je bent dood. Het woord was "
+                  Pick    -> "Kies een letter"
+                  Used    -> "Gebruikte letters: "
+strings EN m = case m of
+                  Another -> "Play another game? [Y/n]"
+                  Won     -> "Congratulations! You got it!"
+                  Lost    -> "You're dead. The word was "
+                  Pick    -> "Pick a letter"
+                  Used    -> "Used letters: "
 
-turn :: State -> Char -> State
-turn state letter
-   | not  $ isAlpha letter                       = samestate InvalidLetter
-   | elem (toUpper letter) $ guessed state = samestate AlreadyGuessed
-   | won  $ newstate Win                         = newstate  Win
-   | lost $ newstate Loss                        = newstate  Loss
-   | otherwise                                   = newstate  Okay
-   where samestate :: Condition -> State
-         samestate cond =
-             State { condition = cond
-                   , word      = word state
-                   , progress  = progress state
-                   , tries     = tries state
-                   , guessed   = guessed state
-                   }
-         newstate :: Condition -> State
-         newstate cond =
-             State { condition = cond
-                   , word      = word state
-                   , progress  = zipWith
-                        (||)
-                        (progress state)
-                        guess
-                   , tries     = if or guess
-                                    then tries state
-                                    else tries state + 1
-                   , guessed   = toUpper letter : guessed state
-                   }
-         guess :: [Bool]
-         guess = map ((==) $ toUpper letter) $ (map toUpper $ word state)
-         won :: State -> Bool
-         won state = and $ progress state
-         lost :: State -> Bool
-         lost state = tries state >= (length hangman - 1)
+data Message = Another | Won | Lost | Pick | Used
 
-data State = State { condition      :: Condition
-                   , word           :: String
-                   , progress       :: [Bool]
-                   , tries          :: Int
-                   , guessed        :: [Char]
-                   } deriving (Eq, Show)
-
-data Condition = Okay
-               | InvalidLetter
-               | AlreadyGuessed
-               | Loss
-               | Win
-               deriving (Eq, Show)
+data Language = NL
+              | EN
+              deriving (Show)
 
 hangman = [ unlines  [ ""
                      , ""
@@ -212,21 +172,3 @@ hangman = [ unlines  [ ""
                      , "|___"
                      ]
           ]
-
-strings :: Language -> String -> String
-strings NL s = case s of
-                  "Another" -> "Wil je nog een keer spelen? [y/n]"
-                  "Won"     -> "Gefeliciteerd! Je hebt het woord geraden!"
-                  "Lost"    -> "Je bent dood. Het woord was "
-                  "Pick"    -> "Kies een letter"
-                  "Used"    -> "Gebruikte letters: "
-strings EN s = case s of
-                  "Another" -> "Play another game? [y/n]"
-                  "Won"     -> "Congratulations! You got it!"
-                  "Lost"    -> "You're dead. The word was "
-                  "Pick"    -> "Pick a letter"
-                  "Used"    -> "Used letters: "
-
-data Language = NL
-              | EN
-              deriving (Show)
